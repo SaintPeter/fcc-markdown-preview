452989bfe127fa14f9cc243bff0c1d8aa547bbef
SaintPeter <rex.schrader@gmail.com>
2020-08-31 15:00:25 -0700

    Basic Markdown Preview

---
+++ src/components/MarkdownPreview.vue
@@ -0,0 +1,68 @@
+<template>
+  <div id="container">
+    <div class="column">
+      <h2>Markdown</h2>
+      <textarea id="editor" @keyup="textarea_keyup" v-model="content"></textarea>  
+    </div>
+    
+    <div class="column">
+      <h2>Preview</h2>
+      <div id="preview" v-html="content_markdown"></div>  
+    </div>
+  </div>
+</template>
+
+
+<script>
+  const marked = require('marked');
+  marked.setOptions({
+    breaks: true
+  })
+
+  export default {
+    name: 'MarkdownPreview',
+    data: function () {
+      this.content = "";
+      this.content_markdown = "";
+      return {
+        content: this.content,
+        content_markdown: this.content_markdown
+      }
+    },
+    methods: {
+      textarea_keyup: function () {
+        this.content_markdown = marked(this.content);
+      }        
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  
+  #container {
+    display: flex;
+    height: 80vh;
+  }
+  
+  .column {
+    box-sizing: border-box;
+    width: 50%;
+    margin: 5px;
+    flex-grow: 1;
+  }
+  
+  #editor {
+    box-sizing: border-box;
+    height: 100%;
+    width: 100%;
+  }
+  
+  #preview {
+    box-sizing: border-box;
+    height: 100%;
+    width: 100%;
+    border: 1px solid black;
+    padding: 5px;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,30 @@ import App from './App.vue'
 
 Vue.config.productionTip = false
 
+/*
+let store = {
+  debug: true,
+  state: {
+    content: 'Hello!'
+  },
+  setMessageAction (newValue) {
+    if (this.debug) console.log('setMessageAction triggered with', newValue)
+    this.state.message = newValue
+  },
+  clearMessageAction () {
+    if (this.debug) console.log('clearMessageAction triggered')
+    this.state.message = ''
+  }
+}
+*/
+
 new Vue({
   render: h => h(App),
+
 }).$mount('#app')
+
+/*   data: {
+    sharedState: store.state
+  }
+  
+ */

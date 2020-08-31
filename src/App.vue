cc598246bb7e2a86f87b841b8c74a5262c13fc35
SaintPeter <rex.schrader@gmail.com>
2020-08-31 15:00:41 -0700

    Basic Markdown Preview

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <MarkdownPreview/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MarkdownPreview from './components/MarkdownPreview.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MarkdownPreview
   }
 }
 </script>
@@ -21,8 +20,16 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 30px;
 }
+
+h1, h2, h3, h4, h5, h6 {
+  margin-top: 0px;
+  margin-bottom: 10px;
+}
+  p {
+    margin: 0px auto 0px auto;
+  }
+  
 </style>
